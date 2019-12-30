150ca68dd28850109ecb2fefb7a768e720ba1f47
Titusz Ban <titusz.ban@trayport.com>
2019-12-30 18:08:57 +0000

    Improve readabilty with a switch-case

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -5,10 +5,10 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
-        private static string _agedBrie = "Aged Brie";
-        private static string _backstagePasses = "Backstage passes to a TAFKAL80ETC concert";
-        private static string _sulfuras = "Sulfuras, Hand of Ragnaros";
-        private static string _conjured = "Conjured Mana Cake";
+        private const string _agedBrie = "Aged Brie";
+        private const string _backstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        private const string _sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string _conjured = "Conjured Mana Cake";
 
         public GildedRose(IList<Item> Items)
         {
@@ -26,31 +26,29 @@ namespace csharp
 
         private static int GetQualityChangeBeforeSellInUpdate(Item item)
         {
-            if (item.Name == _sulfuras) return 0;
-
-            if (item.Name == _agedBrie) return 1;
-
-            if (item.Name == _backstagePasses) return item.SellIn <= 5 ? 3 : (item.SellIn <= 10 ? 2 : 0);
-
-            if (item.Name == _conjured) return -2;
-
-            return -1;
+            switch (item.Name)
+            {
+                case _sulfuras: return 0;
+                case _agedBrie: return 1;
+                case _backstagePasses: return item.SellIn <= 5 ? 3 : (item.SellIn <= 10 ? 2 : 0);
+                case _conjured: return -2;
+                default: return -1;
+            }
 
         }
 
         private static int GetQualityChangeAfterSellInUpdate(Item item)
         {
             if (item.SellIn >= 0) return 0;
-            
-            if (item.Name == _sulfuras) return 0;
-            
-            if (item.Name == _agedBrie) return 1;
 
-            if (item.Name == _backstagePasses) return -item.Quality;
-
-            if (item.Name == _conjured) return -2;
-
-            return -1;
+            switch (item.Name)
+            {
+                case _sulfuras: return 0;
+                case _agedBrie: return 1;
+                case _backstagePasses: return -item.Quality;
+                case _conjured: return -2;
+                default: return -1;
+            }
         }
 
         private static void UpdateItem(Item item)

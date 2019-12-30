cd642f4d13552d0b6ae1d63ca3be0051538b06ba
Titusz Ban <titusz.ban@trayport.com>
2019-12-30 17:04:33 +0000

    Merge nested ifs

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -16,12 +16,9 @@ namespace csharp
             {
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality > 0)
+                    if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                 }
                 else
@@ -32,20 +29,14 @@ namespace csharp
 
                         if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (item.SellIn < 11)
+                            if (item.SellIn < 11 && item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
 
-                            if (item.SellIn < 6)
+                            if (item.SellIn < 6 && item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
@@ -62,12 +53,9 @@ namespace csharp
                     {
                         if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (item.Quality > 0)
+                            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
+                                item.Quality = item.Quality - 1;
                             }
                         }
                         else
